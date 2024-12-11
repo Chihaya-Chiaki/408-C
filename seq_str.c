f0eb6bbfdb47a302441c736c33b2c2bfdb848a32
Mygo <030925fhr@gmail.com>
2024-12-11 23:27:09 +0800

    一个小更新

--- seq_str.c
+++ seq_str.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int a,b;
-    float c;
-    a=b=c=5.2;
+//     int a,b;
+//     float c;
+//     a=b=c=5.2;
 
-//printf("%d\t%d\t%d\n",a,b,c);
-printf("%d\t%d\t%d\n",a,b,(int)c);
-//printf("%f\t%f\t%f\n",a,b,c);
-printf("%f\t%f\t%f\n",(float)a,(float)b,c);
+// //printf("%d\t%d\t%d\n",a,b,c);
+// printf("%d\t%d\t%d\n",a,b,(int)c);
+// //printf("%f\t%f\t%f\n",a,b,c);
+// printf("%f\t%f\t%f\n",(float)a,(float)b,c);
+
+    //char c='A';
+    //char c; c='A';
+    char c=65;
+    printf("%c\n",c);
 
     return 0;
 }
